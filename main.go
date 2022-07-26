4193433d22f40a1e045812c2751b0f61ffa2cf07
Sanskar Jain <sanskar.jain1@swiggy.in>
2022-07-26 15:26:48 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Data struct {
+	Name string `json:"name"`
+	ID   int64  `json:id`
+}
+
+func ReqeustHandler(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	dataReturn := Data{
+		Name: "Sanskar Jain",
+		ID:   35,
+	}
+	dataAccept := Data{}
+	switch req.Method {
+	case "GET":
+		err := json.NewEncoder(w).Encode(dataReturn)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	case "POST":
+		err := json.NewDecoder(req.Body).Decode(&dataAccept)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, "The input recieved is=>%+v", dataAccept)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", ReqeustHandler)
+	http.ListenAndServe(":8080", nil)
+}
